ce8bad6a6507f30208a7e760d38d526f1c917d02
Akash-Anandhan <akashasha.as@gmail.com>
2024-09-21 18:03:14 +0530

    learned about the types of varibales in javascript

--- day_1/app.js
+++ day_1/app.js
@@ -5,3 +5,20 @@ console.log('hello world')
 
 console.warn('this is a warning')
 console.error('a serious error is caought ')
+ //  for comments 
+
+ // variables
+ let akash_age = 10;
+ console.log(akash_age)
+
+ // var variables - normal variable 
+var asha = 'asha'
+console.log(asha)
+
+ // const variable - only one assignment
+ const akash= 'akash'
+console.log(akash)
+akash= 'aaksh1'
+console.log(akash)
+
+
