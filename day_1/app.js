1223c9967bfe5b0d5b23f47375b2a62f5970802e
Akash-Anandhan <akashasha.as@gmail.com>
2024-09-21 18:14:07 +0530

    learned how to create numbers and do arithmetic operations in them

--- day_1/app.js
+++ day_1/app.js
@@ -16,9 +16,21 @@ var asha = 'asha'
 console.log(asha)
 
  // const variable - only one assignment
- const akash= 'akash'
+const akash= 'akash'
 console.log(akash)
-akash= 'aaksh1'
-console.log(akash)
+//akash= 'aaksh1'
+//console.log(akash)
+
+// primitive data types in java
+// number i  javaScript and arithmetic opertaions 
+let num1 = 10 ;
+let num2 =  20 ;
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 ** num2)
+console.log(typeof num1)
+console.log(typeof akash)
 
 
