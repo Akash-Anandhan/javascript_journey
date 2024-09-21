08591099bfb33597d31c2e79e882dd530797e129
Akash-Anandhan <akashasha.as@gmail.com>
2024-09-21 19:03:54 +0530

    learned about control flow statements - if and else if and else

---
+++ day_2/app.js
@@ -0,0 +1,13 @@
+//control statements 
+let a,b
+a=10
+b=10
+if (a<b){
+    console.log('b is grater than a ')
+}
+else if (a>b){
+    console.log('a is grater than b')
+}
+else{
+    console.log('a is equal to b')
+}
