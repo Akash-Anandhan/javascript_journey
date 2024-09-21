0dad5be04f8d39598a0e335f9eaa5cc88be2314f
Akash-Anandhan <akashasha.as@gmail.com>
2024-09-21 18:27:40 +0530

    learned about relational and equaltiy operators

--- day_1/app.js
+++ day_1/app.js
@@ -33,4 +33,15 @@ console.log(num1 ** num2)
 console.log(typeof num1)
 console.log(typeof akash)
 
+// comparison opertaors - 2 types 
+// relational operators - returs true or false 
+console.log(num1<num2)
+console.log(num1 > num2)
+console.log(num1 <= num2)
+console.log(num1 >= num2)
 
+//equality operators 
+let num3 = '10';
+console.log(num1 == num3) // lose equality 
+console.log(num1===num3) // strict equality 
+console.log(num != num2) // not equal to
