9e3e55ad4842d03c1795059e787d16a6b36f4bb3
Akash-Anandhan <akashasha.as@gmail.com>
2024-09-22 22:14:40 +0530

    learned about switch acse and for loop in javascript

---
+++ day_3/app.js
@@ -0,0 +1,19 @@
+//switch case in javascript
+let x= "tuesdays"
+switch(x){
+    case "monday":
+        console.log("today is monday")
+        break
+    case "tuesday":
+        console.log("today is tuesday")
+        break
+    case "wednesday":
+        console.log("today is wenesday")
+        break
+    default:
+        console.log("no data mathes to the actual switch case condition")
+}
+//for loop in javascript
+for (let i=0;i<=6;i++){
+    console.log("welcome")
+}
