35b62ae68dfe78cf183b4f1ae6df63915d39c916
Akash-Anandhan <akashasha.as@gmail.com>
2024-09-23 21:14:09 +0530

    learned about array functions in javascript

--- day_4/app.js
+++ day_4/app.js
@@ -2,4 +2,23 @@
 let arr = [1,2,3,3,45,,6]
 let arr1 =[] //empty array
 //acsesing array items
-console.log(arr[3])
+console.log(arr[4])
+//array functions 
+//1.concat
+let arr2=[2,3,4,5,6,]
+let arr3= arr.concat(arr2)
+console.log(arr3)
+//2.shift
+arr.shift()
+console.log(arr)
+//3.pop()
+arr.pop()
+console.log(arr)
+//4.inculdes
+console.log(arr.includes(1))
+//5.slice
+console.log(arr.slice(0,4))
+//6.join
+console.log(arr.join('-'))
+//7.reverse
+console.log(arr.reverse())
