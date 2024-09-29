d4f44328720968b5aaa7bbec73cad8eb27df632a
Akash-Anandhan <akashasha.as@gmail.com>
2024-09-30 00:15:14 +0530

    learned to use time and setinterval and settimeout function in java script

--- app.js
+++ app.js
@@ -15,3 +15,33 @@ console.log(person1)
 // converting a string back to json(object)
 const person2 = JSON.parse(person1)
 console.log(person2)
+
+//time manipulations 
+const todate = new Date()
+console.log(todate)
+// date formating 
+//1.only required parts of the date like year date second and millisecond 
+console.log(`Year : ${todate.getFullYear()}`)
+console.log(`Day : ${todate.getDay()}`)
+console.log(`Date : ${todate.getDate()}`)
+console.log(`Second : ${todate.getSeconds()}`)
+console.log(`Millisecond : ${todate.getMilliseconds()}`)
+
+
+//2. desired formatting 
+console.log(`Localdatestring : ${todate.toLocaleDateString()}`)
+console.log(`ISO_string : ${todate.toISOString()}`)
+console.log(`Datestring : ${todate.toDateString()}`)
+
+
+// time manipulation program with setinterval
+const name = setInterval(
+function akash(){
+    console.log("akash")
+},1000
+)
+
+setTimeout(function akash(){
+    console.log("interval is being cleared")
+    clearInterval(name)    
+}, 5000);
