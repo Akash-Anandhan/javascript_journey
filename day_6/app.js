d5b6b9855f5de9acb3d9897c2c9a74db7348b177
Akash-Anandhan <akashasha.as@gmail.com>
2024-10-03 21:53:03 +0530

    learned about default parameter passing technique in javascript

---
+++ day_6/app.js
@@ -0,0 +1,8 @@
+//passing default parameter to a function
+function multiply(a,b=7){
+    console.log(`the ans is ${a*b}`)
+}
+// passing only a argumet and letting b as default one 
+multiply(3)
+//passing both arguments 
+multiply(2,3)
