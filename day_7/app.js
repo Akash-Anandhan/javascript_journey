8d654db6bef4794d5e87c42389ae942b1bdfa2fb
Akash-Anandhan <56512934+Akash-Anandhan@users.noreply.github.com>
2025-05-01 22:39:05 +0530

    minor change

--- day_7/app.js
+++ day_7/app.js
@@ -6,6 +6,6 @@ function guy(fname, lname, ...hobies){
         console.log("i like ",hobies[i])
     }   
 }
-// using rest operator to pass an array with n number of i0tems into a function
-arr=["aksh","anandhan","drawing","writhing","dancing"]
+// using rest operator to pass an array with n number of i0tems into a function.
+arr=["aksh","anandhan","drawing","writhing","dancing","dancing"]
 guy(...arr)
